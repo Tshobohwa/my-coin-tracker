e4371dd27d19a1b3608750b9688e5edcb31d4ef2
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-22 08:25:25 +0200

    Create categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.includes(:expenses).where({ user_id: current_user.id })
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    icon = category_param[:icon]
+    name = category_param[:name]
+    @category = Category.new(name:, icon:, user_id: current_user.id)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category added successfully'
+    else
+      flash.now[:error] = @category.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def category_param
+    params.require(:category).permit(:name, :icon)
+  end
+end
